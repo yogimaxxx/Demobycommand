78f028450a300bddffd76ad0071cf55ee48dcbf7
jkmohite96 <jkmohite96@gmail.com>
2017-08-10 12:44:07 +0530

    First Commit by cmd

---
+++ liveinjava/liveinjava/src/main/java/com/liveinjava/bean/MavenDemo.java
@@ -0,0 +1,18 @@
+package com.liveinjava.bean;
+
+public class MavenDemo {
+/**
+ * @author PrashantChitte
+ * @param a
+ * @param b
+ */
+	
+	int result;
+	
+	
+	public void calculate(int a,int b)
+	{
+		 result=a+b;
+		 System.out.println("Addition="+result);
+	}
+}
